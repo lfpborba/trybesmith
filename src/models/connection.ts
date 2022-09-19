782095fb41f1b1b6071ed432e773e1b468c797e6
lfpborba <luisfpb@icloud.com>
2022-09-19 18:28:42 -0300

    fix: fixing connection

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -7,7 +7,6 @@ const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || '1234',
-  database: process.env.MYSQL_DATABASE || 'Trybesmith',
 });
 
 export default connection;
